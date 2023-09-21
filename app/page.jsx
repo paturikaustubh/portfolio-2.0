50ab63b8f71608eb60855c9f8f238382241d88de
sardonian <plutofeb115@gmail.com>
2023-09-22 00:15:27 +0530

    ADD: contact me form

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import KnowMore from "@components/KnowMore/KnowMore";
+import MessageForm from "@components/MessageForm/MessageForm";
 import { Raleway } from "next/font/google";
 import Link from "next/link";
 const raleway = Raleway({ subsets: ["latin"] });
@@ -56,7 +57,7 @@ export default function Home() {
           className="hover:fill-[#68A063] duration-300 fill-neutral-800"
         >
           <path d="M429.76,130.07,274.33,36.85a37,37,0,0,0-36.65,0L82.24,130.06A38.2,38.2,0,0,0,64,162.83V349a38.26,38.26,0,0,0,18.24,32.8L123,406.14l.23.13c20.58,10.53,28.46,10.53,37.59,10.53,32.14,0,52.11-20.8,52.11-54.29V182a8.51,8.51,0,0,0-8.42-8.58H182.13a8.51,8.51,0,0,0-8.42,8.58V362.51a15,15,0,0,1-6.85,13.07c-5.9,3.6-14.47,2.84-24.14-2.15l-39.06-23.51a1.1,1.1,0,0,1-.48-.92V165.46a1.32,1.32,0,0,1,.59-1.06l151.84-93a.82.82,0,0,1,.73,0l151.93,93a1.34,1.34,0,0,1,.55,1.1V349a1.28,1.28,0,0,1-.45,1L256.31,440.65a1.22,1.22,0,0,1-.8,0l-38.83-23.06a7.8,7.8,0,0,0-7.83-.41l-.34.2c-10.72,6.35-13.6,8-23.54,11.62-1.62.59-5.43,2-5.76,5.77s3.29,6.45,6.51,8.32l51.9,31.87a35.67,35.67,0,0,0,18.3,5.07l.58,0h0a35.87,35.87,0,0,0,17.83-5.07l155.43-93.13A38.37,38.37,0,0,0,448,349V162.83A38.21,38.21,0,0,0,429.76,130.07Z" />
-          <path d="M307.88,318.05c-37.29,0-45.24-10.42-47.6-27.24a8.43,8.43,0,0,0-8.22-7.32h-19.8a8.44,8.44,0,0,0-8.26,8.58c0,14.58,5.12,62.17,83.92,62.17h0c24.38,0,44.66-5.7,58.63-16.49S388,311.26,388,292.55c0-37.55-24.5-47.83-72.75-54.55-49.05-6.82-49.05-10.29-49.05-17.89,0-5.47,0-18.28,35.46-18.28,25.23,0,38.74,3.19,43.06,20a8.35,8.35,0,0,0,8.06,6.67h19.87a8.24,8.24,0,0,0,6.16-2.86,8.91,8.91,0,0,0,2.12-6.44c-2.57-35.55-28.56-53.58-79.24-53.58-46.06,0-73.55,20.75-73.55,55.5,0,38.1,28.49,48.87,71.29,53.33,50,5.17,50,12.71,50,19.37C349.46,304.2,345.15,318.05,307.88,318.05Z" />
+          <path d="M307.88,318.05c-37.29,0-45.24-10.42-47.6-27.24a8.43,8.43,0,0,0-8.22-7.32h-19.8a8.44,8.44,0,0,0-8.26,8.58c0,14.58,5.12,62.17,83.92,62.17h0c24.38,0,44.66-5.7,58.63-20.49S388,311.26,388,292.55c0-37.55-24.5-47.83-72.75-54.55-49.05-6.82-49.05-10.29-49.05-17.89,0-5.47,0-18.28,35.46-18.28,25.23,0,38.74,3.19,43.06,20a8.35,8.35,0,0,0,8.06,6.67h19.87a8.24,8.24,0,0,0,6.16-2.86,8.91,8.91,0,0,0,2.12-6.44c-2.57-35.55-28.56-53.58-79.24-53.58-46.06,0-73.55,20.75-73.55,55.5,0,38.1,28.49,48.87,71.29,53.33,50,5.17,50,12.71,50,19.37C349.46,304.2,345.15,318.05,307.88,318.05Z" />
         </svg>
       ),
     },
@@ -134,7 +135,7 @@ export default function Home() {
           className="hover:fill-[#68A063] duration-300 fill-neutral-800"
         >
           <path d="M429.76,130.07,274.33,36.85a37,37,0,0,0-36.65,0L82.24,130.06A38.2,38.2,0,0,0,64,162.83V349a38.26,38.26,0,0,0,18.24,32.8L123,406.14l.23.13c20.58,10.53,28.46,10.53,37.59,10.53,32.14,0,52.11-20.8,52.11-54.29V182a8.51,8.51,0,0,0-8.42-8.58H182.13a8.51,8.51,0,0,0-8.42,8.58V362.51a15,15,0,0,1-6.85,13.07c-5.9,3.6-14.47,2.84-24.14-2.15l-39.06-23.51a1.1,1.1,0,0,1-.48-.92V165.46a1.32,1.32,0,0,1,.59-1.06l151.84-93a.82.82,0,0,1,.73,0l151.93,93a1.34,1.34,0,0,1,.55,1.1V349a1.28,1.28,0,0,1-.45,1L256.31,440.65a1.22,1.22,0,0,1-.8,0l-38.83-23.06a7.8,7.8,0,0,0-7.83-.41l-.34.2c-10.72,6.35-13.6,8-23.54,11.62-1.62.59-5.43,2-5.76,5.77s3.29,6.45,6.51,8.32l51.9,31.87a35.67,35.67,0,0,0,18.3,5.07l.58,0h0a35.87,35.87,0,0,0,17.83-5.07l155.43-93.13A38.37,38.37,0,0,0,448,349V162.83A38.21,38.21,0,0,0,429.76,130.07Z" />
-          <path d="M307.88,318.05c-37.29,0-45.24-10.42-47.6-27.24a8.43,8.43,0,0,0-8.22-7.32h-19.8a8.44,8.44,0,0,0-8.26,8.58c0,14.58,5.12,62.17,83.92,62.17h0c24.38,0,44.66-5.7,58.63-16.49S388,311.26,388,292.55c0-37.55-24.5-47.83-72.75-54.55-49.05-6.82-49.05-10.29-49.05-17.89,0-5.47,0-18.28,35.46-18.28,25.23,0,38.74,3.19,43.06,20a8.35,8.35,0,0,0,8.06,6.67h19.87a8.24,8.24,0,0,0,6.16-2.86,8.91,8.91,0,0,0,2.12-6.44c-2.57-35.55-28.56-53.58-79.24-53.58-46.06,0-73.55,20.75-73.55,55.5,0,38.1,28.49,48.87,71.29,53.33,50,5.17,50,12.71,50,19.37C349.46,304.2,345.15,318.05,307.88,318.05Z" />
+          <path d="M307.88,318.05c-37.29,0-45.24-10.42-47.6-27.24a8.43,8.43,0,0,0-8.22-7.32h-19.8a8.44,8.44,0,0,0-8.26,8.58c0,14.58,5.12,62.17,83.92,62.17h0c24.38,0,44.66-5.7,58.63-20.49S388,311.26,388,292.55c0-37.55-24.5-47.83-72.75-54.55-49.05-6.82-49.05-10.29-49.05-17.89,0-5.47,0-18.28,35.46-18.28,25.23,0,38.74,3.19,43.06,20a8.35,8.35,0,0,0,8.06,6.67h19.87a8.24,8.24,0,0,0,6.16-2.86,8.91,8.91,0,0,0,2.12-6.44c-2.57-35.55-28.56-53.58-79.24-53.58-46.06,0-73.55,20.75-73.55,55.5,0,38.1,28.49,48.87,71.29,53.33,50,5.17,50,12.71,50,19.37C349.46,304.2,345.15,318.05,307.88,318.05Z" />
         </svg>
       ),
     },
@@ -187,20 +188,20 @@ export default function Home() {
   return (
     <section className="flex flex-col">
       <div className="h-screen grid grid-cols-12">
-        <div className="col-span-5 bg-violet-600 h-full flex justify-end items-center">
+        <div className="lg:col-span-5 col-span-4 bg-violet-600 h-full flex justify-end items-center">
           <img
             src={"Images/Kaustubh.jpg"}
             className="kaustubh-img border lg:translate-x-1/4"
           />
         </div>
-        <div className="col-span-7 flex items-center justify-center text-neutral-800">
-          <div className="flex flex-col">
+        <div className="lg:col-span-7 col-span-8 flex items-center justify-center text-neutral-800">
+          <div className="flex flex-col lg:pl-24">
             <p className={`${raleway.className} text-7xl`}>
               <span className="font-bold text-violet-600">Kaustubh </span>
               <span className="font-light">Paturi</span>
             </p>
             <p
-              className={`${raleway.className} text-2xl`}
+              className={`${raleway.className} text-neutral-600 text-2xl`}
               style={{ fontStyle: "italic" }}
             >
               Nothing great ever came that easy.
@@ -214,14 +215,14 @@ export default function Home() {
         </div>
       </div>
       <div className="grid grid-cols-12" id="about-me">
-        <div className="bg-violet-600 col-span-5">
+        <div className="bg-violet-600 lg:col-span-5 col-span-4">
           <p
-            className={`${raleway.className} text-7xl font-bold text-center py-16 text-white sticky top-1/4 fade`}
+            className={`${raleway.className} text-7xl font-bold text-center pb-20 text-white sticky top-1/4 fade`}
           >
             About Me
           </p>
         </div>
-        <div className="col-span-7 flex w-[60ch] mx-auto items-center py-16">
+        <div className="lg:col-span-7 col-span-8 flex w-[60ch] mx-auto items-center pb-20">
           <div className="flex flex-col">
             <p className="text-xl text-neutral-800 about-me-text fade fade-translate">
               <span>Hello there! Kaustubh here, a </span>
@@ -255,14 +256,17 @@ export default function Home() {
         </div>
       </div>
       <div className="grid grid-cols-12">
-        <div className="pt-32 pb-16 bg-violet-600 col-span-5" id="skills">
+        <div
+          className="py-20 bg-violet-600 lg:col-span-5 col-span-4"
+          id="skills"
+        >
           <p
             className={`${raleway.className} text-7xl font-bold text-center text-white sticky top-1/4 fade`}
           >
             Skills
           </p>
         </div>
-        <div className="col-span-7 pt-32 pb-16 flex flex-col justify-center items-center">
+        <div className="lg:col-span-7 col-span-8 py-20 flex flex-col justify-center items-center">
           <p className="text-5xl text-neutral-800 about-me-text font-light fade">
             <span className="text-violet-600 font-bold">Front End</span> Skills
           </p>
@@ -288,14 +292,17 @@ export default function Home() {
         </div>
       </div>
       <div className="grid grid-cols-12">
-        <div className="py-16 bg-violet-600 col-span-5" id="projects">
+        <div
+          className="py-20 bg-violet-600 lg:col-span-5 col-span-4"
+          id="projects"
+        >
           <p
             className={`${raleway.className} text-7xl font-bold text-center text-white sticky top-1/4 fade`}
           >
             Projects
           </p>
         </div>
-        <div className="col-span-7 py-16 flex flex-col justify-center items-center">
+        <div className="lg:col-span-7 col-span-8 py-20 flex flex-col justify-center items-center">
           <div className="w-[75ch]">
             <div className="fade">
               <p className="text-4xl font-bold text-[#DC3545]">Wok of Fame</p>
@@ -303,7 +310,7 @@ export default function Home() {
                 Restraunt website for ordering food
               </p>
             </div>
-            <div className="mt-4 fade rounded-md border-2 group shadow-lg relative project-img">
+            <div className="mt-1 fade rounded-md border-2 group shadow-lg relative project-img">
               <img src="Images/Wok of Fame.png" width={"100%"} />
               <div className="project-actions">
                 <Link
@@ -329,7 +336,7 @@ export default function Home() {
               </p>
               <p className="text-xl text-neutral-500">IMDb clone website</p>
             </div>
-            <div className="mt-4 fade rounded-md border-2 group shadow-lg relative project-img">
+            <div className="mt-1 fade rounded-md border-2 group shadow-lg relative project-img">
               <img src="Images/Flikipedia.png" width={"100%"} />
               <div className="project-actions">
                 <Link
@@ -348,6 +355,23 @@ export default function Home() {
                 </Link>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-12">
+        <div
+          className="py-20 bg-violet-600 lg:col-span-5 col-span-4"
+          id="about-me"
+        >
+          <p
+            className={`${raleway.className} text-7xl font-bold text-center text-white sticky top-1/4 fade`}
+          >
+            Contact Me
+          </p>
+        </div>
+        <div className="lg:col-span-7 col-span-8 py-20 flex flex-col justify-center items-center">
+          <div className="w-[75ch]">
+            <MessageForm />
           </div>
         </div>
       </div>
---
+++ components/MessageForm/MessageForm.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+export default function MessageFunction() {
+  return (
+    <form
+      className="border-2 rounded-md p-4 grid grid-cols-12 gap-3 bg-neutral-100 fade"
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <p className="text-4xl col-span-12 font-bold text-violet-600 fade">
+        Message Me
+      </p>
+      <div className="col-span-12 text-lg max-w-[45ch] fade">
+        {`Got something in your mind to share? Express yourself here!\nStart by introducing yourself`}
+      </div>
+      <input
+        type="text"
+        className="form-input col-span-6 fade"
+        required
+        placeholder="Full Name"
+      />
+      <input
+        type="email"
+        className="form-input col-span-6 fade"
+        required
+        placeholder="E-Mail"
+      />
+      <textarea
+        name="message"
+        id="message"
+        className="form-input col-span-12 fade"
+        rows={3}
+        placeholder="Enter your message"
+      />
+      <button
+        type="submit"
+        className="text-right text-accent-500 py-2 px-6 hover:text-white hover:bg-accent-500 duration-300 rounded-md ml-auto col-span-12 border-2 border-accent-500 cursor-pointer text-lg fade"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
